203c0ebbbddbebc517738b77eccc0c1215ee5bf4
Dylan Chen <dylanachen@users.noreply.github.com>
2026-01-16 18:09:48 +0000

    Lab1: Added task_1.py script for name-greeting

---
+++ Lab1/DylanChen_6984540266/scripts/task_1.py
@@ -0,0 +1,10 @@
+"""
+Author: Dylan Chen
+Description: User name greeting script.
+"""
+
+# Prompt user for name
+name = input("Please enter your name: ")
+
+# Printing the name greeting
+print(f"Hello, {name}!")
